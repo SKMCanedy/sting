fdaa03c12a34e6a4a632dc3156e3a7ba1965c0ed
Shannon Canedy <git@mikidee.com>
2018-05-20 00:12:18 -0700

    flesh out dashboard html and add username field to landing page modal

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const router = express.Router();
 const port =  process.env.PORT || 8080;
 
-app.use(express.static('public'), function (req,res){
+app.use("/", express.static("public"), function (req,res){
     res.sendStatus(200);
-})
+});
+
+app.use("/dashboard", express.static("app"), function (req,res){
+    res.sendStatus(200);
+});
+
+// app.get('/dashboard', function(req, res) {
+//     res.sendFile(path.join(__dirname + '/app/index.html'));
+// });
+
 
 app.listen(port, function (){
     console.log(`Listening on port ${port}`);
